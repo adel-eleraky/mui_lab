7f5d08d344b63ad889db74e720535a3757b66435
el3raky <kameladel339@gmail.com>
2025-02-25 10:39:54 +0200

    finished mui form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,7 +20,6 @@ function Form() {
 
 
     const handleSelect = (event, value) => {
-        // setCountry(event.target.value)
         setFormData({ ...formData, country: event.target.value })
     }
 
@@ -30,18 +29,39 @@ function Form() {
         setFormData({ ...formData, [key]: value })
     }
 
-    
+    const handleCheckbox = (event, value) => {
+        const index = formData.skills.indexOf(event.target.value)
+        if (index === -1) {
+            setFormData({...formData, skills: [...formData.skills, event.target.value]})
+        }
+        else {
+            let newSkills = formData.skills.filter((val) => val !== event.target.value)
+            setFormData({...formData, skills: newSkills})
+        }
+    }
+
+    const handleRadio = (event) => {
+        if(event.target.checked){
+            setFormData({...formData, gender: event.target.name})
+        }
+    }
+
+
+    const resetForm = (() => {
+        setFormData({ username: "", email: "", password: "", country: "", skills: [] , gender: "" })
+    })
+
     console.log(formData)
     return (
         <>
-            
+
             <Box component={"form"} sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '50%', margin: '50px auto' }}>
                 <Grid container justifyContent={"space-between"} spacing={2}>
                     <Grid item xs={12} md={6}>
-                        <TextField variant='outlined' label="username" name="username" sx={{ width: "100%" }} onChange={handleForm}></TextField>
+                        <TextField variant='outlined' value={formData.username} label="username" name="username" sx={{ width: "100%" }} onChange={handleForm}></TextField>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <TextField variant='outlined' label="email" name="email" sx={{ width: "100%" }} onChange={handleForm}></TextField>
+                        <TextField variant='outlined' value={formData.email} label="email" name="email" sx={{ width: "100%" }} onChange={handleForm}></TextField>
                     </Grid>
                     <Grid item xs={12} md={6}>
                         <FormControl variant="outlined" sx={{ width: "100%" }}>
@@ -67,6 +87,7 @@ function Form() {
                                 label="Password"
                                 onChange={handleForm}
                                 name="password"
+                                value={formData.password}
                             />
                         </FormControl>
                     </Grid>
@@ -90,13 +111,13 @@ function Form() {
                             <FormLabel>Skills</FormLabel>
                             <Grid container spacing={2}>
                                 <Grid item xs={12} sm={4} md={3}>
-                                    <FormControlLabel onChange={handleCheckbox} checked={formData.skills.includes("HTML")} control={<Checkbox />} label="HTML" />
+                                    <FormControlLabel onChange={handleCheckbox} checked={formData.skills.includes("HTML")} control={<Checkbox />} label="HTML" value="HTML" />
                                 </Grid>
                                 <Grid item xs={12} sm={4} md={3}>
-                                    <FormControlLabel onChange={handleCheckbox} checked={formData.skills.includes("CSS")} control={<Checkbox />} label="CSS" />
+                                    <FormControlLabel onChange={handleCheckbox} checked={formData.skills.includes("CSS")} control={<Checkbox />} label="CSS" value="CSS" />
                                 </Grid>
                                 <Grid item xs={12} sm={4} md={3}>
-                                    <FormControlLabel onChange={handleCheckbox} checked={formData.skills.includes("JAVASCRIPT")} control={<Checkbox />} label="JAVASCRIPT" />
+                                    <FormControlLabel onChange={handleCheckbox} checked={formData.skills.includes("JAVASCRIPT")} control={<Checkbox />} label="JAVASCRIPT" value="JAVASCRIPT" />
                                 </Grid>
                             </Grid>
                         </FormGroup>
@@ -107,10 +128,10 @@ function Form() {
                             <RadioGroup>
                                 <Grid container>
                                     <Grid item xs={12} md={6}>
-                                        <FormControlLabel control={<Radio value={"male"} />} label="Male" />
+                                        <FormControlLabel onChange={handleRadio} name="Male" checked={formData.gender == "Male"} control={<Radio value={"male"} />} label="Male" />
                                     </Grid>
                                     <Grid item xs={12} md={6}>
-                                        <FormControlLabel control={<Radio value={"female"} />} label="female" />
+                                        <FormControlLabel onChange={handleRadio} name="Female" checked={formData.gender == "Female"} control={<Radio value={"female"} />} label="female" v />
                                     </Grid>
                                 </Grid>
                             </RadioGroup>
@@ -120,7 +141,7 @@ function Form() {
                         <Button variant="contained" sx={{ width: "100%" }}>submit</Button>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Button variant="outlined" color="error" sx={{ width: "100%" }}>reset</Button>
+                        <Button onClick={resetForm} variant="outlined" color="error" sx={{ width: "100%" }}>reset</Button>
                     </Grid>
                 </Grid>
             </Box>
